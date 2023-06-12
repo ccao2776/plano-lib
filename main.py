ce5a35ed2ca69532f78eedfc9146b577a414e523
Charles Cao <charles@Charless-MBP.attlocal.net>
2023-06-12 18:40:37 -0500

    study room indexing

--- main.py
+++ main.py
@@ -24,23 +24,48 @@ def main():
     # close resident form
     popup_form_close = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-primary']")))
     popup_form_close.click()
-
+    
+    time.sleep(1)
     # select library
     libraries_deselect = driver.find_element(By.XPATH, "//label[normalize-space()='Check all']")
     libraries_deselect.click()
-
     selected_library = '\'' + selected_library + ' Library' + '\'' + ")]"
-
     library_button = driver.find_element(By.XPATH, "//div[@class='amnp-location-select']//label[contains(text()," + selected_library)
     library_button.click()
 
-    print('\'')
+    time.sleep(2)
+    handle_scheduling(driver)
+
+
+    # input login
+    # form_username = driver.find_element(By.XPATH, "//input[@id='login_card']")
+    # form_username.send_keys('USERNAME')
+    # form_password = driver.find_element(By.XPATH, "//input[@id='login_pin']")
+    # form_password.send_keys('PASSWORD')
+    # form_continue = driver.find_element(By.XPATH, "//span[@class='btn btn-edit amnp-lookup input-group-addon']")
+    # form_continue.click()
 
     time.sleep(20)
 
-    # based on
+
+# create a greedy scheduling algorithm based on 11am-12PM start timeframe
+def handle_scheduling(driver):
+    rooms = driver.find_elements(By.XPATH, "//div[@class='amnp-room-holder']")
+    study_rooms = []
+
+    # append WebElements that contains parasable studyrooms
+    for room in rooms:
+        heading = room.find_element(By.TAG_NAME, "h3")
+        if "Study" in heading:
+            study_rooms.append(room)
+    
+    # schedule based on first room availability
+    for study_room in study_rooms:
+        
     
 
+def handle_information_form():
+    pass
 
 def usage():
     pass
